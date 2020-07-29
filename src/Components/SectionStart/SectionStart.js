150420c20d01bafa2ce88fb2bd97e64aed80fef2
Paweł Politowski <matelly@poczta.onet.pl>
2020-07-29 20:58:59 +0200

    First section done

--- src/Components/SectionStart/SectionStart.js
+++ src/Components/SectionStart/SectionStart.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Fingers from '../../images/Fingers.png';
 import './SectionStart.css';
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faAngleDoubleDown} from '@fortawesome/free-solid-svg-icons';
 
 function SectionStart() {
     return (
@@ -10,7 +12,8 @@ function SectionStart() {
                 <p>Lorem ipsum dolor sit amer, consectetur adipiscing elit, sed do eiusmod tempor inciddunt ut labore et dolore magna aliqua.
                 Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maeceneas accumsan lacus vel facilisis.</p>
                 <button className="aboutApp">O aplikacji</button>
-                <button className="download">Pobierz</button>           
+                <button className="download">Pobierz</button>
+                <button className="scrollDown"><FontAwesomeIcon icon={faAngleDoubleDown}/></button>           
             </div>
             <img src={Fingers} alt="fingers"/>                
         </div>
