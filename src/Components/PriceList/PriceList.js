dbcc0340667b357ded436dd1655e25a6b0ab6baf
Paweł Politowski <matelly@poczta.onet.pl>
2020-07-30 22:31:28 +0200

    Section third almosr done

--- src/Components/PriceList/PriceList.js
+++ src/Components/PriceList/PriceList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './PriceList.css';
 import Phone from '../../images/phone.png';
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCheck} from '@fortawesome/free-solid-svg-icons';
 
 
 function PriceList() {
@@ -16,19 +18,78 @@ function PriceList() {
                         <img src={Phone} alt="phone"/>
                         <h3>Lorem ipsum<h1>Dolor</h1></h3>
                     </div>
-                    <div>
+                    <div className="middleSectionLeft">
                         <h1>Za darmo</h1>
                         <h3>na zawsze!</h3>
                     </div>
+                    <div className="bottomSectionLeft">
+                        <div><FontAwesomeIcon icon={faCheck} style={{color:"goldenrod"}}/><span>Lorem ipsum</span></div>
+                        <div><FontAwesomeIcon icon={faCheck} style={{color:"goldenrod"}}/><span>dolor sit amet, consectetur</span></div>
+                        <div><FontAwesomeIcon icon={faCheck} style={{color:"goldenrod"}}/><span>adipiscing elit, sed do eiusmod</span></div>
+                        <div><FontAwesomeIcon icon={faCheck} style={{color:"goldenrod"}}/><span>tempor incididunt</span></div>
+                        <div><FontAwesomeIcon icon={faCheck} style={{color:"goldenrod"}}/><span>ut labore et dolore</span></div>
+                        <div><FontAwesomeIcon icon={faCheck} style={{color:"goldenrod"}}/><span>magna aliqua</span></div>
+                    </div>  
                 </div>
                 <div className="rightSide">  
                     <div className="titleRightSide">
                         <img src={Phone} alt="phone"/>
                         <h3>Lorem ipsum<h1>dolor sit amet</h1></h3>
+                    </div>  
+                    <div className="middleSectionRight">
+                        <div>
+                            <h3 style={{color:"goldenrod"}}>Basic</h3>                            
+                            <h3>wersja<br/>darmowa</h3>
+                        </div>
+                        <div>
+                            <h3 style={{color:"goldenrod"}}>Premium</h3>
+                            <select>
+                                <option value="90 dni">90 dni</option>
+                            </select>                            
+                            <h3 style={{paddingRight:0}}>79,99zł<span className="pricePrgh" style={{color:"green"}}>-30% taniej</span></h3>                             
+                        </div>                      
                     </div>
-                    <div>
-                        <h1>Za darmo</h1>
-                        <h3>na zawsze!</h3>
+                    <div className="bottomSectionRight">
+                        <div className="bottomSectionRightPrgh">
+                            <div className="text">Lorem ipsum</div>
+                            <div className="icon"><FontAwesomeIcon icon={faCheck} style={{color:"goldenrod"}}/></div>                            
+                            <div className="icon"><FontAwesomeIcon icon={faCheck} style={{color:"goldenrod"}}/></div> 
+                        </div>
+                        <div className="bottomSectionRightPrgh">
+                            <div className="text">dolor sit amet, consectetur</div>
+                            <div className="icon"><FontAwesomeIcon icon={faCheck} style={{color:"goldenrod"}}/></div>                            
+                            <div className="icon"><FontAwesomeIcon icon={faCheck} style={{color:"goldenrod"}}/></div> 
+                        </div>
+                        <div className="bottomSectionRightPrgh">
+                            <div className="text">adipiscing elit, sed do eiusmod</div>
+                            <div className="icon"><FontAwesomeIcon icon={faCheck} style={{color:"goldenrod"}}/></div>                            
+                            <div className="icon"><FontAwesomeIcon icon={faCheck} style={{color:"goldenrod"}}/></div> 
+                        </div>
+                        <div className="bottomSectionRightPrgh">
+                            <div className="text">tempor incididunt</div>
+                            <div className="icon"><FontAwesomeIcon icon={faCheck} style={{color:"goldenrod"}}/></div>                            
+                            <div className="icon"><FontAwesomeIcon icon={faCheck} style={{color:"goldenrod"}}/></div> 
+                        </div>
+                        <div className="bottomSectionRightPrgh">
+                            <div className="text">ut labore et dolore</div>
+                            <div className="icon"><FontAwesomeIcon icon={faCheck} style={{color:"goldenrod"}}/></div>                            
+                            <div className="icon"><FontAwesomeIcon icon={faCheck} style={{color:"goldenrod"}}/></div> 
+                        </div>
+                        <div className="bottomSectionRightPrgh">
+                            <div className="text">Lorem ipsum</div>
+                            <div className="icon"><FontAwesomeIcon icon={faCheck} style={{color:"goldenrod"}}/></div>                            
+                            <div className="icon"><FontAwesomeIcon icon={faCheck} style={{color:"goldenrod"}}/></div> 
+                        </div>
+                        <div className="bottomSectionRightPrgh">
+                            <div className="text">Ipsum dolor sit amet</div>
+                            <div className="icon"><FontAwesomeIcon icon={faCheck} style={{color:"goldenrod"}}/></div>                            
+                            <div className="icon"><FontAwesomeIcon icon={faCheck} style={{color:"goldenrod"}}/></div> 
+                        </div>
+                        <div className="bottomSectionRightPrgh">
+                            <div className="text">ut labore et dolore magna aliqua</div>
+                            <div className="icon"><FontAwesomeIcon icon={faCheck} style={{color:"goldenrod"}}/></div>                            
+                            <div className="icon"><FontAwesomeIcon icon={faCheck} style={{color:"goldenrod"}}/></div> 
+                        </div>
                     </div>
                 </div>
             </div>
